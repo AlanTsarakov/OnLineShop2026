c968e1023950cb8d2538322b535e5f11f9ddeadf
Мария МД <mahadrum@yandex.ru>
2026-03-03 09:54:17 +0300

    Добавьте файлы проекта.

---
+++ OnLineShop2026/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace OnLineShop2026.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Cost { get; set; }
+        
+        public Product(string name, string description, decimal cost)
+        {
+            Name = name;
+            Description = description;
+            Cost = cost;
+        }
+
+        public override string ToString()
+        {
+            return $"Товар {Name}, Описание {Description}, Цена {Cost}";
+        }
+
+    }
+}
