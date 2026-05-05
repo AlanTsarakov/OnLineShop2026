39be61dd50dafba3dfcffb1c661bbada81cf562d
AlanTsarakov <alantsarakov@vk.com>
2026-05-05 22:49:20 +0300

    Выполнено. Подзадача 1. Создать модель для описания пользователя

---
+++ OnLineShop2026/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnLineShop2026.Models
+{
+    internal class User
+    {
+        public User()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public User(string? name, string? surname, string? email, string? urlAvatar) : this()
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            UrlAvatar = urlAvatar;
+        }
+
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        public string? UrlAvatar { get; set; }
+    }
+}
