bf40c50d4bb42fa81f0e2b793ef48a8b5305b56d
AlanTsarakov <alantsarakov@vk.com>
2026-05-05 22:54:28 +0300

    Выполнено. Подзадача 2. Создать контроллер, где создается один пользователь и передается во View.

---
+++ OnLineShop2026/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnLineShop2026.Data;
+using OnLineShop2026.Models;
+
+namespace OnLineShop2026.Controllers
+{
+    public class UserController : Controller
+    {
+        //IProductRepository productsRepository;
+
+        //public UserController(IProductRepository prodRep)
+        //{
+        //    this.productsRepository = prodRep;
+        //}
+        public IActionResult Index()
+        {
+            User user = new User("Алан", "Цараков", "alantsarakov@vk.com", null);
+            return View(User);
+        }
+    }
+}
